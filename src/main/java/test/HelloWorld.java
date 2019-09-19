0d00af50059018bb5afb1bb3bdd370b6182d53a2
basava <basavapunnaiah81@gmail.com>
2019-09-19 10:10:59 +0530

    "aoaasam"

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("PLEASE Enter Your Name Here");
+		System.out.println("hello");
 		Scanner sc= new Scanner(System.in);
 		System.out.println("This is sample application");
 		String name= sc.nextLine();
